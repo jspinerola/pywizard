8be36fc0138894a0dbbc0ab932d3fae62cf52ae9
jspinerola <144980273+jspinerola@users.noreply.github.com>
2025-11-24 16:45:49 -0600

    feat: made footer

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router";
 import Nav from "./Nav";
+import Footer from "./Footer";
 
 function Layout() {
   return (
@@ -9,7 +10,7 @@ function Layout() {
       <main className="w-full mx-auto container m-6">
         <Outlet />
       </main>
-      <div>Footer</div>
+      <Footer />
     </>
   );
 }
