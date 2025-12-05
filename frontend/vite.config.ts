4eb42060b966899ea0ac121d0f4d0e035b990dfe
Kevin A Hernandez Garcia <kevinalbertohernandezgarcia@gmail.com>
2025-12-05 15:16:25 -0600

    fixes: for deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,14 @@ import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import checker from "vite-plugin-checker";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(), 
+    tailwindcss(),
+    checker({ typescript: false }),],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
